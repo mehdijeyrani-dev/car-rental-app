4757852f44347e9076da2d449d632837ddaa4923
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2025-10-17 14:23:13 +0330

    refactor(components): clean up Title and About components

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
+const desc =
+  "Find reliable car with transparent pricing, verified inspections, flexible pickup and delivery options, and 24/7 customer support for a smooth rental or buying experience.";
+
 const Title = ({
-  title1,
-  title2,
-  titleStyles,
-  title2Styles,
-  paraStyles,
-  para,
+  subTitle = "",
+  heading = "",
+  subTitleClassName = "",
+  headingClassName = "",
+  description = desc,
+  descriptionClassName = "",
 }) => {
   return (
-    <div className={titleStyles}>
-      <h4 className="text-solid capitalize">{title1}</h4>
+    <div className={subTitleClassName}>
+      <h4 className="text-solid capitalize">{subTitle}</h4>
       <div className="flex flex-col xl:flex-row xl:justify-between">
-        <h1 className={`${title2Styles} capitalize`}>{title2}</h1>
+        <h1 className={`${headingClassName} capitalize`}>{heading}</h1>
         <p
-          className={`${paraStyles} max-w-lg xl:place-self-end xl:relative xl:bottom-3`}
+          className={`${descriptionClassName} max-w-lg xl:place-self-end xl:relative xl:bottom-3`}
         >
-          {para
-            ? para
-            : "Find reliable car with transparent pricing, verified inspections, flexible pickup and delivery options, and 24/7 customer support for a smooth rental or buying experience."}
+          {description}
         </p>
       </div>
     </div>
